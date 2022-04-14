bd17c8aef827b3477dc17f12e935ec549202c604
SxxHyun <shp35079@naver.com>
2022-04-14 19:32:33 +0900

    [CHORE] context provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Main from "./pages/Main";
 import GlobalStyle from "./styles/GlobalStyle";
 import "antd/dist/antd.css";
+import { UserProvider } from "./contexts";
 
 function App() {
   return (
-    <>
+    <UserProvider>
       <GlobalStyle />
       <Main />
-    </>
+    </UserProvider>
   );
 }
 
